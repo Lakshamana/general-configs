1fc97faac7aa41e92a8fe3e4da691e46c9586c4c
Lakshamana <lakshamana@protonmail.com>
2022-12-15 15:41:22 -0300

    chore: rename configure to configure.sh

--- configure.sh
+++ configure.sh
@@ -30,8 +30,61 @@ log() {
       fi
 }
 
+# prompt user for yes/no question and return choice
+# 1) Question: string
+# 2) default value: y/n (string)
+_prompt_user() {
+      default_value=$2
+      prompt_choice='y/n'
+
+      if [[ $default_value == 'y' ]]
+      then
+            prompt_choice='Y/n'
+      elif [[ $default_value == 'n' ]]
+      then
+            prompt_choice='y/N'
+      fi
+
+      read -p "$1 [$prompt_choice]: " choice
+
+      valid_choices=('y', 'n')
+
+      if [[ $choice == '' ]]
+      then
+            echo $default_value
+      fi
+
+      if ! [[ ${valid_choices[*]} =~ $choice ]]
+      then
+            _prompt_user "$1" "$2"
+      fi
+
+      echo $choice
+}
+
+# wrapper function for _prompt_user
+prompt_user() {
+      echo `_prompt_user "$1" "$2" | head -n1 | awk '{print $1}'`
+}
+
+# install iosevka font
+install_font() {
+      git clone --depth=1 https://github.com/Lakshamana/iosevka-docker.git ~/.iosevka-custom
+      cd ~/.iosevka-custom
+
+      # build iosevka docker image and run, extract iosevka-custom ttf files
+      docker build -t iosevka_build . -f Dockerfile
+      docker run -e -it -v $(pwd)/build:/build iosevka_build ttf::iosevka-custom
+
+      # copy files to fonts folder
+      cp -r build/dist/* /usr/share/fonts/
+      fc-cache
+}
+
 log 'installing necessary packages...'
 sudo pacman -Sy \
+      xorg \
+      mesa \
       maim \
       python \
       git \
@@ -46,11 +99,15 @@ sudo pacman -Sy \
       zathura-pdf-mupdf \
       neovim \
       vim \
-      chezmoi
-
-# setup alacritty
-cp alacritty/.alacritty.yml ~
-log 'setup alacritty'
+      chezmoi \
+      i3-gaps \
+      i3blocks \
+      i3exit \
+      i3lock \
+      i3status \
+      pavucontrol \
+      docker \
+      docker-compose
 
 # restart i3
 i3-msg restart
@@ -86,13 +143,18 @@ yay -Sy asdf-vm
 asdf plugin-add nodejs
 
 # install nodejs
-asdf install nodejs $NODEJS_VERSION
 asdf install nodejs lts
 log 'setup nodejs asdf env'
 
 log 'downloading vim-plug now...'
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-vim -c ':source ~/.config/nvim/init.vim'
-vim -c ':PlugInstall'
+nvim -c ':source ~/.config/nvim/init.vim'
+nvim -c ':PlugInstall'
 log 'nvim plugins successfully installed...'
+
+# should I install iosevka-custom font?
+prompt_result=`prompt_user "Install iosevka-custom font?" 'y'`
+if [[ $prompt_result == 'y' ]]; then install_font fi
+
+
